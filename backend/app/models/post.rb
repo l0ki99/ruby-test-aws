6e14081b70467ced1dc9909c2bc74bb7eafeabf3
rhys <rhys@higherit.ca>
2026-03-19 12:09:30 -0400

    Rename Post#users to Post#commenters to disambiguate from post author

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :users, through: :comments
+  has_many :commenters, class_name: "User", through: :comments, source: :user
 
   validates :title, presence: true
   validates :content, presence: true
