1b6ea202eb0f3dd53818a25295d9bcdef5596bfa
rhys <rhys@higherit.ca>
2026-03-19 10:39:31 -0400

    Backfill image_url in migration with cat and dog images

--- backend/db/migrate/20260319134641_add_image_url_to_posts.rb
+++ backend/db/migrate/20260319134641_add_image_url_to_posts.rb
@@ -1,5 +1,16 @@
 class AddImageUrlToPosts < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_column :posts, :image_url, :string
+
+    execute <<~SQL
+      UPDATE posts SET image_url = '/cat.png' WHERE title = 'Lorem Ipsum'
+    SQL
+    execute <<~SQL
+      UPDATE posts SET image_url = '/dog.png' WHERE title = 'Turbo Encabulator'
+    SQL
+  end
+
+  def down
+    remove_column :posts, :image_url
   end
 end
